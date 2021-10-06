062f7152f5c02aa9e256989c76799ec4ea0d374f
Hailey <91793891+River-99@users.noreply.github.com>
2021-10-06 16:30:38 +0900

    Add files via upload

---
+++ exer1-19.c
@@ -0,0 +1,25 @@
+//#include <stdio.h>
+//
+//void reverse(char* s);
+//int length (char* s);
+//
+//int main()
+//{
+//	reverse("ABCDEF");
+//	return 0;
+//}
+//
+//int length(char* s) {
+//	int tmp = 0;
+//	while (s[tmp] != '\0') {
+//		tmp++;
+//	}
+//	return tmp;
+//}
+//void reverse(char* s) {
+//	int len = length(s);
+//	while (len > 0) {
+//		printf("%c", s[len-1]);
+//		len--;
+//	}
+//}
